bfe56f891cb67d5e355dce2d7460306093d3f85b
jerrychai <jerryisadoofus@gmail.com>
2025-09-15 11:41:16 -0400

    first change

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,7 +1,16 @@
 public class DataTypes {
-    // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
+
+    public static void main(String[] args) {
+        long x = 500_000_500_000L;
+        int[] numbers = new int[100000];
+        for (int i = 1; i <= 100000; i++) {
+            numbers[i-1] = i;
+        }
+        System.out.println(DataTypes.sum(numbers));
+    }
+
     public static long sum(int[] numbers) {
-        int s = 0; // variable to accumulate the sum in!
+        long s = 0; // variable to accumulate the sum in!
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
             s += x;
